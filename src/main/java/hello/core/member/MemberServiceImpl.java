cb16a3f673119deef747c6182a6e9df50770db7d
suwhan2 <jerry5230@naver.com>
2024-03-17 16:47:55 +0900

    feat : member domain

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+
+    //가입을 하고 회원을 찾기 위해서 앞에서 만들었던 저장소가 필요
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+            //구현체 없이 null로 놔두면 오류 발생 -> 구현 객체를 선택해줘야 함
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+        //다형성에 의해서 인터페이스가 아니라 MemoryMemberRepository에 있는 override한 save가 호출
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+    //저장소가 확정되지 않아서 memorymemberrepository로 그냥 개발
+    //테스트만 가능
+    private static Map<Long, Member> store = new HashMap<>();
+    //동시성 이슈가 있을 수 있으므로 ConcurrentHashMap을 써야한다(실무에서는)
+    //연습이니 간단하게
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
