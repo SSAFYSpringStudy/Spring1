2f5bf54bdd23f65bb56925dada3e45eb93f64bac
suwhan2 <jerry5230@naver.com>
2024-03-18 07:48:55 +0900

    refactor : add SpringContainer

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,17 +8,25 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration //애플리케이션 구성정보를 담당하는 설정정보라는 뜻
 public class AppConfig {
+    //Bean을 달아주면 Spring컨테이너에 등록된다.
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
-    private  MemoryMemberRepository memberRepository() {
+    @Bean
+    public  MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,18 +4,27 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     //순수한 자바 코드
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-//        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        //Spring은 모두 ApplicationContext에서 시작 = Spring컨테이너라고 생각하자, 모든걸 관리
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //AppConfig의 설정정보를 가지고 관리함
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class); //이름 정해서 꺼내기(이름, 타입)
+
+
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Member findMember = memberService.findMember(1l);
+        Member findMember = memberService.findMember(1L);
         System.out.println("new member = " + member.getName());
         System.out.println("find Member = " + findMember.getName());
 
